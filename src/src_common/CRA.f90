99092176194353ed52d8dea641d8a5ea02148f45
Müller  Jonathan <jonmuell@ethz.ch>
2026-06-30 17:20:44 +0200

    Updated headers

--- src/src_common/CRA.f90
+++ src/src_common/CRA.f90
@@ -1,3 +1,36 @@
+!***************************************************************************
+! CRA.f90
+! -------
+! Copyright © 2026, ETH Zurich, Jonathan Muller
+!
+! This file is part of EddyFlow®.
+!
+! EddyFlow (TM) is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version. You should have received a copy
+! of the GNU General Public License along with EddyFlow (R). If not,
+! see <http://www.gnu.org/licenses/>.
+!
+! EddyFlow® contains additional Open Source Components. The licenses
+! and/or notices these Components can be found in the file LIBRARIES.txt.
+!
+! EddyFlow® is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+! GNU General Public License for more details.
+!
+!***************************************************************************
+!
+! \brief       Applies centered running average smoothing.
+! \author      Jonathan Muller
+! \note
+! \sa
+! \bug
+! \deprecated
+! \test
+! \todo
+!***************************************************************************
 subroutine CRA(Set, nrow, ncol, fs, tconst, fcol)
     use m_common_global_var
     implicit none
