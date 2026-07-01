6bbb8e0f4df8d40b6b27b41dafc24381425952e2
kebasaa <muellerjonathan@gmx.net>
2026-07-01 09:42:51 +0200

    Update changelog

--- prj/current_date_and_version_as_parameters.sh
+++ prj/current_date_and_version_as_parameters.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 timestamp=`date "+%Y-%m-%d, %H:%M"`
-version='7.1.0'
+version='7.2.2'
 echo "character(5), parameter  :: sw_ver = '"$version"'" > ../src/src_common/version_and_date.inc
 echo "character(17), parameter :: build_date = '"$timestamp"'" >> ../src/src_common/version_and_date.inc
 touch ../src/src_common//init_env.f90
